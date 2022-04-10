6536d7b53eeba90741b183ff9350f47705644439
Irina <ideryusheva@inbox.ru>
2022-04-10 17:56:20 +0700

    Практическое задание №1

--- HomeWork1/HomeWork1.1/Program.cs
+++ HomeWork1/HomeWork1.1/Program.cs
@@ -1,14 +1,16 @@
-﻿int numberA = 7, numberB = 22;
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int numberA = 7, numberB = 7;
 
 if (numberA > numberB)
 {
-    Console.WriteLine($"Максимальное число: {numberA}.");
+    Console.WriteLine($"Максимальное число: {numberA}, минимальное число: {numberB}.");
 }
 else
 {
-    Console.WriteLine($"Максимальное число: {numberB}.");
+    Console.WriteLine($"Максимальное число: {numberB}, минимальное число: {numberA}.");
 }
 if (numberA == numberB)
 {
-    Console.WriteLine("Нет максимального числа.");
+    Console.WriteLine("Числа равны.");
 }
