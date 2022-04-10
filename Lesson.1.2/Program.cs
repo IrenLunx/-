5d2213855abfe0cd57f5c1abccef83bf441efd00
Irina <ideryusheva@inbox.ru>
2022-04-10 15:24:53 +0700

    Семинар 1.2 + задание в группах

---
+++ Lesson.1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA, numberB;
+
+numberA = 5;
+numberB = 25;
+
+if (numberB == numberA *numberA)
+{
+    Console.WriteLine($"Число {numberB} является квадратом числа {numberA}");
+}
+else 
+{
+    Console.WriteLine($"Число {numberB} не является квадратом числа {numberA}");
+}
