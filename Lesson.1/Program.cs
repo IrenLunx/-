aae05d12ba4cc8c1af8edf2fe08df441867df6f6
Irina <ideryusheva@inbox.ru>
2022-04-10 14:46:09 +0700

    Семинар 1

---
+++ Lesson.1/Program.cs
@@ -0,0 +1,11 @@
+﻿int number, numberSquare;
+
+number = -11;
+numberSquare = number * number;
+
+Console.WriteLine($"Квадрат {number} равен {numberSquare}");
+
+number = -115734;
+numberSquare = number * number;
+
+Console.WriteLine($"Квадрат {number} равен {numberSquare}");
