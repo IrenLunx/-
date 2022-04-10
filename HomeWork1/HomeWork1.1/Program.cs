bce6d923c7c4fefadfea3cc40a71856a52c8d593
Irina <ideryusheva@inbox.ru>
2022-04-10 18:22:31 +0700

    Практическое задание №2

--- HomeWork1/HomeWork1.1/Program.cs
+++ HomeWork1/HomeWork1.1/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-int numberA = 7, numberB = 7;
+int numberA = 9, numberB = 8;
 
-if (numberA > numberB)
+if (numberA > numberB) 
 {
     Console.WriteLine($"Максимальное число: {numberA}, минимальное число: {numberB}.");
 }
-else
+if (numberB > numberA)
 {
     Console.WriteLine($"Максимальное число: {numberB}, минимальное число: {numberA}.");
 }
--- HomeWork1/HomeWork1.2/Program.cs
+++ HomeWork1/HomeWork1.2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int numberA = 20, numberB = 100, numberC = 10000, max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine($"Максимальное число: {max}!");
