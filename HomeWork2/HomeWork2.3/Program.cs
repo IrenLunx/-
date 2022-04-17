38c46f9f45595c3bdcc33813d3270e54d9dd30af
Irina <ideryusheva@inbox.ru>
2022-04-17 09:41:12 +0700

    Домашняя работа №2

--- HomeWork2/HomeWork2.3/Program.cs
+++ HomeWork2/HomeWork2.3/Program.cs
@@ -1,2 +1,17 @@
 ﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+int number = new Random().Next(1, 100000);
+Console.Write("В числе " + number);
+
+if (number < 100)
+    System.Console.WriteLine(" третьего числа нет!");
+
+while (number > 99)
+{ 
+    if (number < 1000)
+    {
+        Console.WriteLine(" третья цифра - " + (number % 10));
+        break;
+    }
+    number = number / 10;
+}
