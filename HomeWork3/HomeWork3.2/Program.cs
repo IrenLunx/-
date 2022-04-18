52cb03796526ebd2364210be7cce2f0935d94c81
Irina <ideryusheva@inbox.ru>
2022-04-18 23:17:20 +0700

    Домашняя работа №3

---
+++ HomeWork3/HomeWork3.2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double xa = 4, ya = 8, za = 10, xb = 1, yb = 5, zb = 7;
+double spacing = (xa - xb) * (xa - xb) + (ya - yb) * (ya - yb) + (za - zb) * (za - zb);
+spacing = Math.Sqrt(spacing);
+Console.WriteLine("Расстояние между точками = " + "{0:f3}", spacing);
+
---
+++ HomeWork3/HomeWork3.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int n = new Random().Next(2, 10);
+System.Console.Write($"Наше число - {n}! Кубы чисел от 1 до {n}: ");
+int i = 1;
+int kub;
+
+while (i < n)
+{
+    kub = i * i * i;
+    Console.Write($"{kub}, ");
+    i++;
+}
+if (i == n)
+{
+    kub = i * i * i;
+    Console.Write($"{kub}.");
+}
