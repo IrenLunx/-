28eb036d9fcac54494e5400497891ee5878bd9c9
Irina <ideryusheva@inbox.ru>
2022-04-19 19:59:41 +0700

    Домашняя работа №3

--- HomeWork3/HomeWork3.1/Program.cs
+++ HomeWork3/HomeWork3.1/Program.cs
@@ -1,12 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-int number = 56865;
-if (number % 10 == number / 10000)
-{
-    if (number / 10 % 10 == number / 1000 % 10)
-        System.Console.WriteLine($"Число {number} является палиндромом!");
-    else
-        System.Console.WriteLine($"Число {number} не является палиндромом!");
-}
+int number = 57865;
+if (number % 10 == number / 10000 && number / 10 % 10 == number / 1000 % 10)
+    System.Console.WriteLine($"Число {number} является палиндромом!");
 else
     System.Console.WriteLine($"Число {number} не является палиндромом!");
--- HomeWork3/HomeWork3.3/Program.cs
+++ HomeWork3/HomeWork3.3/Program.cs
@@ -1,14 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-int n = new Random().Next(2, 10);
-System.Console.Write($"Наше число - {n}! Кубы чисел от 1 до {n}: ");
+int n = new Random().Next(1, 100);
+System.Console.WriteLine($"Наше число - {n}! Кубы чисел от 1 до {n}: ");
 int i = 1;
 int kub;
 
 while (i < n)
 {
     kub = i * i * i;
-    Console.Write($"{kub}, ");
+    if (i % 10 == 0)
+        System.Console.WriteLine($"{kub}, ");
+    else
+        Console.Write($"{kub}, ");
     i++;
 }
 if (i == n)
