2d827fad586e8b4e284b0837411a2747b3e10a1d
Irina <ideryusheva@inbox.ru>
2022-04-19 19:31:20 +0700

    Домашняя работа №3

--- HomeWork3/HomeWork3.1/Program.cs
+++ HomeWork3/HomeWork3.1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+int number = 56865;
+if (number % 10 == number / 10000)
+{
+    if (number / 10 % 10 == number / 1000 % 10)
+        System.Console.WriteLine($"Число {number} является палиндромом!");
+    else
+        System.Console.WriteLine($"Число {number} не является палиндромом!");
+}
+else
+    System.Console.WriteLine($"Число {number} не является палиндромом!");
