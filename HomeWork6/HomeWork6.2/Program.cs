1263e31e4f1c5bd1fbb4acc1da0b0d1ff00a3a4a
Irina <ideryusheva@inbox.ru>
2022-05-07 22:33:52 +0700

    исправление дз

--- HomeWork6/HomeWork6.2/Program.cs
+++ HomeWork6/HomeWork6.2/Program.cs
@@ -10,12 +10,13 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 System.Console.Write("Введите значение b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * (b2 - b1) / (k1 - k2) + b1;
-
 if (k1 == k2 && b1 == b2)
     System.Console.WriteLine("Прямые совпадают! Точек пересечения бесконечное количество.");
 else if (k1 == k2)
-    System.Console.WriteLine("Прямые линии параллельны! Точек пересечения нет.");    
+    System.Console.WriteLine("Прямые линии параллельны! Точек пересечения нет.");  
 else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
     System.Console.WriteLine($"Точка пересечения двух прямых: x = {x}, y = {y}");
+}
