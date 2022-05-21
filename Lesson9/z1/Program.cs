6d0cc6e470802af445f4dd863b3244ab93974a4f
Irina <ideryusheva@inbox.ru>
2022-05-21 17:17:59 +0700

    Lesson 9

---
+++ Lesson9/z1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void PrintToNumber(int currentNum, int maxVoid)
+{
+    if (currentNum <= maxVoid)
+    {
+        if (currentNum < maxVoid)
+            Console.Write(currentNum + ", ");
+        else
+            Console.WriteLine(currentNum + " ");
+        currentNum++;
+        PrintToNumber(currentNum, maxVoid);
+    }
+}
+
+int n = 5;
+int i = 1;
+PrintToNumber(i, n);
+
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void PrintNums(int maxNum, int currentNum = 1)
+{
+    if(currentNum <= maxNum)
+    {
+        string symbol = ", ";
+        if(currentNum == maxNum)
+        {
+            symbol = ".";
+        }
+        Console.Write(currentNum + symbol);
+        currentNum++;
+        PrintNums(maxNum, currentNum);
+    }
+}
+
+PrintNums(m);
---
+++ Lesson9/z2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+void PrintToNumber(int currentNum, int maxVoid)
+{
+    if (currentNum <= maxVoid)
+    {
+        if (currentNum < maxVoid)
+            Console.Write(currentNum + ", ");
+        else
+            Console.WriteLine(currentNum + " ");
+        currentNum++;
+        PrintToNumber(currentNum, maxVoid);
+    }
+}
+
+int n = 10;
+int m = 5;
+PrintToNumber(m, n);
